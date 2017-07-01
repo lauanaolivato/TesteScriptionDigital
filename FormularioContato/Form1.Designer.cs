867b83da1265cd83607fc8ee230870d38ee216b9
lauanaolivato <lauana.olivato@hotmail.com>
2017-07-01 16:50:28 -0300

    first commit

---
+++ FormularioContato/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FormularioContato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnedd = new System.Windows.Forms.Button();
+            this.btndele = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(543, 347);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(28, 399);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(128, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = " Novo Contato";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnedd
+            // 
+            this.btnedd.Location = new System.Drawing.Point(199, 399);
+            this.btnedd.Name = "btnedd";
+            this.btnedd.Size = new System.Drawing.Size(128, 23);
+            this.btnedd.TabIndex = 4;
+            this.btnedd.Text = "Editar";
+            this.btnedd.UseVisualStyleBackColor = true;
+            // 
+            // btndele
+            // 
+            this.btndele.Location = new System.Drawing.Point(374, 399);
+            this.btndele.Name = "btndele";
+            this.btndele.Size = new System.Drawing.Size(128, 23);
+            this.btndele.TabIndex = 5;
+            this.btndele.Text = "Deletar";
+            this.btndele.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 434);
+            this.Controls.Add(this.btndele);
+            this.Controls.Add(this.btnedd);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contato";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnedd;
+        private System.Windows.Forms.Button btndele;
+    }
+}
+
---
+++ FormularioContato/frmAddEditContato.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace FormularioContato
+{
+    partial class frmAddEditContato
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnomecontato = new System.Windows.Forms.TextBox();
+            this.txttelefone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnsave
+            // 
+            this.btnsave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnsave.Location = new System.Drawing.Point(175, 159);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 3;
+            this.btnsave.Text = "Salvar";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome Do Contato";
+            this.label1.UseWaitCursor = true;
+            // 
+            // txtnomecontato
+            // 
+            this.txtnomecontato.Location = new System.Drawing.Point(116, 9);
+            this.txtnomecontato.Name = "txtnomecontato";
+            this.txtnomecontato.Size = new System.Drawing.Size(229, 20);
+            this.txtnomecontato.TabIndex = 0;
+            this.txtnomecontato.UseWaitCursor = true;
+            // 
+            // txttelefone
+            // 
+            this.txttelefone.Location = new System.Drawing.Point(115, 44);
+            this.txttelefone.Name = "txttelefone";
+            this.txttelefone.Size = new System.Drawing.Size(229, 20);
+            this.txttelefone.TabIndex = 1;
+            this.txttelefone.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefone";
+            this.label4.UseWaitCursor = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(115, 83);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(229, 20);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Email";
+            this.label2.UseWaitCursor = true;
+            // 
+            // frmAddEditContato
+            // 
+            this.AcceptButton = this.btnsave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 237);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txttelefone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtnomecontato);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnsave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmAddEditContato";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add/ Editar Contato";
+            this.UseWaitCursor = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmAddEditContato_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnomecontato;
+        private System.Windows.Forms.TextBox txttelefone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+    }
+}
